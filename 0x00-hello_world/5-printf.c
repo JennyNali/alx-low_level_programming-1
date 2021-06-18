f7a875e0ba2cbaba7ca42d616abd0ab16be4e8c1
root <solomonle>
2021-06-17 18:10:55 -0700

    printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * puts - To write a new line or string to the output. It prints
+ * the passed string with a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
