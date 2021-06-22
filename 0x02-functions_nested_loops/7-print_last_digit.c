93795c79a48e573518f167bec42616a18ddbb7b7
root <solomonle>
2021-06-22 08:34:27 -0700

    last digit func

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - to print last digit of a number
+ *
+ * @n: the number to be printed
+ *
+ * @lastDigit: last digit
+ *
+ * Return: to return the value of n
+ */
+int print_last_digit(int n)
+{
+int lastDigit;
+lastDigit = n % 10;
+return (lasDigit);
+}
